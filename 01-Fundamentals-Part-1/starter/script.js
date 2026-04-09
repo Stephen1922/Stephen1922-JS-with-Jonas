b500c76a95122bb5e3bb7d12c183cfce547248d7
Stephen1922 <stephen.jerome22@gmail.com>
2026-04-09 19:51:32 +0530

    Adding File

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -24,3 +24,106 @@ let job1= 'programmer';
 let job2= 'teacher';
 
 console.log(myCurrentJob);
+
+let javaScriptIsFun=true;
+console.log(javaScriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+//console.log(23);
+//console.log(typeof 'Jonas');
+
+let year;
+console.log(year)
+console.log(typeof year)
+year= 1990
+console.log(typeof year)
+
+
+javaScriptIsFun= 'Yes!'
+console.log(typeof javaScriptIsFun);
+
+console.log(typeof null);
+
+/*Assignment 1*/
+
+let country='India';
+let continent='Asia';
+let population=1000;
+console.log(continent,country,population);
+
+let isIsland=true;
+let language; 
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+/*Different ways to declare variable*/ 
+
+// let, var, const
+
+let age=30;// User can reassign new value to variable. 
+age= "35";
+
+
+/*const lastName="Jerome";  // User cann't reassign new value to variable. 
+lastName= "Nicholas";
+console.log(lastName);
+//const job; // Const variable cannot be empty. 
+*/ 
+
+
+var job="QA";// Try to avoid this. 
+job='Developer';
+console.log(job);
+
+ln='jerome';
+console.log(ln);
+console.log(typeof ln);
+
+
+// Operators
+
+
+// Math Operators
+const ageStephen = 2026-1990;
+const ageDenica = 2026-1991;
+console.log(ageStephen);
+console.log(ageDenica);
+console.log(ageDenica*ageStephen);
+
+const f1="Stephen";
+const f2="Jerome";
+
+console.log(f1 +" "+f2 );
+
+// Assignment operators
+let x = 10+5; //10
+x += 10// x=x+25
+console.log(x);
+x *= 4// x=x*25
+console.log(x);
+x /=20 // x=x/20;
+console.log(x);
+
+
+// Comparasion operators. 
+// < , > , >= , <=
+
+console.log(ageStephen > ageDenica);
+console.log(ageStephen < ageDenica);
+console.log(ageStephen >= 18);
+console.log(ageStephen <= 40);
+
+
+const now = 2037
+const age1 = now - 1991;
+const age2 = now - 2018;
+
+const isFullAge = age1 >= 18;
+
+console.log(age1, age2);
+console.log(isFullAge);
