23491f349a1865cc31910337f898b5280491fecf
Stephen1922 <stephen.jerome22@gmail.com>
2026-04-09 16:24:35 +0530

    Adding File

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+
+      let js = 'amazing';
+      if(js==='amazing') alert('JavaScript is FUN!'); 
+      console.log(40+8+23-10);
+
+      console.log('Shawn');
+      console.log(2);
+
+      let firstName="Jerome";
+      console.log(firstName);
+      console.log(firstName);
+      console.log(firstName);
+
+let stephen_jerome='SJ';
+let $funciton = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob= 'Programmer';
+let myCurrentJob="Teacher";
+
+let job1= 'programmer';
+let job2= 'teacher';
+
+console.log(myCurrentJob);
